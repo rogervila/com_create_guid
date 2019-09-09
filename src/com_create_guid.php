9f89ea5b0438bf33ff941214ab0b6b17887af379
Roger Vilà <rogervila@me.com>
2019-09-09 15:19:40 +0200

    Create com_create_guid.php

---
+++ src/com_create_guid.php
@@ -0,0 +1,19 @@
+<?php
+
+// Original: https://github.com/MicrosoftTranslator/Text-Translation-API-V3-PHP/blob/master/Translate.php#L26
+if (!function_exists('com_create_guid')) {
+    function com_create_guid()
+    {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
+    }
+}
